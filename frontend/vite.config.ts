326a40d41f7c348d46ddee7929004aed0a5f36f4
wahed <wahed.hemati@shikenso.com>
2026-04-06 14:41:03 +0200

    Update .gitignore to include scripts directory and modify Vite config for server settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Required for Amplify
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
+  server: {
+    allowedHosts: true,
   },
 })
